a54c6b0a62b940a927d889f920db019b056dc5c3
qberal <quentin.beral@insa-rouen.fr>
2024-10-21 20:39:01 +0200

    Add dijkstra method

--- src/graphro/mainRO.java
+++ src/graphro/mainRO.java
@@ -4,8 +4,9 @@ package graphro;/*
  * and open the template in the editor.
  */
 
+import java.util.*;
+
 /**
- *
  * @author gdelmondo
  */
 public class mainRO {
@@ -14,20 +15,58 @@ public class mainRO {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-         //graphe G1
-         long IStartTime = System.nanoTime();
-         Graphe g1r = GrapheListe.deFichier("./data/datafig1r.txt");
-         Graphe g1n = GrapheListe.deFichier("./data/datafig1n.txt");
-         //Isomorphic iso = new Isomorphism;
-         System.out.println("*************************************************************** ");
-         System.out.println("Sont les memes G1r & G1r:" + IsomorphismEleve.isIsomorphic(g1r,g1r));
-         System.out.println("Sont les memes G1r & G1n :" + IsomorphismEleve.isIsomorphic(g1r,g1n));
-         long lEndTime = System.nanoTime();
-         long output = lEndTime - IStartTime;
-         System.out.println("Elapsed time in milliseconds:" + output/1000000);
-         System.out.println("*************************************************************** ");
+
+        Graphe grapheInitial = GrapheListe.deFichier("./data/grapheInitial.txt");
+        System.out.println(grapheInitial);
+
+        Sommet startNode = grapheInitial.sommets().iterator().next();
+
+        Map<Sommet, Integer> distances = dijkstra(grapheInitial, startNode);
+
+        for (Map.Entry<Sommet, Integer> entry : distances.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+
     }
 
+    public static Map<Sommet, Integer> dijkstra(Graphe g, Sommet startNode) {
+        Map<Sommet, Integer> distances = new HashMap<>();
+        PriorityQueue<Sommet> pq = new PriorityQueue<>(Comparator.comparingInt(distances::get));
+        Set<Sommet> visited = new HashSet<>();
 
+        // Initialize distances
+        for (Sommet s : g.sommets()) {
+            distances.put(s, Integer.MAX_VALUE);
+        }
+        distances.put(startNode, 0);
+        pq.add(startNode);
+
+        while (!pq.isEmpty()) {
+            Sommet current = pq.poll();
+            if (!visited.add(current)) {
+                continue;
+            }
+
+            for (Sommet neighbor : g.sommets()) {
+                if (g.existeArc(current, neighbor)) {
+                    int newDist = distances.get(current) + g.valeurArc(current, neighbor);
+                    if (newDist < distances.get(neighbor)) {
+                        distances.put(neighbor, newDist);
+                        pq.add(neighbor);
+                    }
+                }
+                // Check the reverse direction for undirected graph
+                if (g.existeArc(neighbor, current)) {
+                    int newDist = distances.get(current) + g.valeurArc(neighbor, current);
+                    if (newDist < distances.get(neighbor)) {
+                        distances.put(neighbor, newDist);
+                        pq.add(neighbor);
+                    }
+                }
+            }
+        }
+
+        return distances;
+    }
 
 }
